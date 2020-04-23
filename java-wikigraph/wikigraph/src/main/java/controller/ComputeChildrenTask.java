6b539e4d90d1d4c3d27c22d6a43b9d63e0afd28b
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-23 22:12:16 +0200

    fix task

--- java-wikigraph/wikigraph/src/main/java/controller/ComputeChildrenTask.java
+++ java-wikigraph/wikigraph/src/main/java/controller/ComputeChildrenTask.java
@@ -1,5 +1,6 @@
 package controller;
 
+import controller.api.HttpWikiGraph;
 import model.WikiGraphNode;
 import model.WikiGraphNodeFactory;
 
@@ -11,13 +12,14 @@ public class ComputeChildrenTask extends CountedCompleter<Void> {
 
     private final String node;
     private final int depth;
+    private final HttpWikiGraph nodeFactory;
 
-    public static void computeChildren(String startNode, int maxDepth){
-        new ComputeChildrenTask(null, startNode, maxDepth).invoke();
+    public static void computeChildren(String startNode, int maxDepth, HttpWikiGraph nodeFactory){
+        new ComputeChildrenTask(null, startNode, maxDepth, nodeFactory).invoke();
     }
 
-    public ComputeChildrenTask(CountedCompleter<?> t,String node, int depth) {
-        super(t);
+    public ComputeChildrenTask(CountedCompleter<?> t,String node, int depth, HttpWikiGraph nodeFactory){
+        this.nodeFactory = nodeFactory;
         this.node = node;
         this.depth = depth;
     }
@@ -25,18 +27,18 @@ public class ComputeChildrenTask extends CountedCompleter<Void> {
 
     @Override
     public void compute() {
-        WikiGraphNodeFactory wikiGraph = new HttpWikiGraph();
         if (depth > 0){
-            WikiGraphNode result = wikiGraph.from(this.node);
-            for(String child:  result.childrenTerms()){
-                addToPendingCount(1);
-                new ComputeChildrenTask(this, child,this.depth-1).fork();
+            WikiGraphNode result = nodeFactory.from(this.node);
+            if(result!=null){
+                for(String child:  result.childrenTerms()){
+                    System.out.println(child);
+                    addToPendingCount(1);
+                    new ComputeChildrenTask(this, child,this.depth-1,this.nodeFactory).fork();
+                }
             }
-
         }
-
-        //propagateCompletion();
-       tryComplete();
+        propagateCompletion();
+       //tryComplete();
     }
 
     @Override
