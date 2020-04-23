e39eb366aeb87f3eab254a23c0e608f611414327
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-23 18:35:09 +0200

    Countedcompleter task base impl

--- java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
@@ -1,6 +1,19 @@
 package controller;
 
+
+import static controller.ComputeChildrenTask.computeChildren;
+
 public class ExecutorController implements Controller {
+
+    private final int depth;
+
+    public ExecutorController(int depth, int poolSize){
+        this.depth= depth;
+
+    }
+    public void compute(String node){
+        computeChildren(node,depth);
+    }
 
     @Override
     public void notifyEvent() {
