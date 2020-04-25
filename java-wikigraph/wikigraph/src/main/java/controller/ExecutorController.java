d11d7cc0cb5e781164657aa2cadf1b7557cd4fcd
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-25 14:55:00 +0200

    update main

--- java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
@@ -60,12 +60,5 @@ public class ExecutorController implements Controller {
         }
     }
 
-    public static void main(String[] args){
-        HttpWikiGraph nodeFactory = new HttpWikiGraph();
-        nodeFactory.setLanguage(Locale.ENGLISH.getLanguage());
-        ConcurrentHashMap<String, WikiGraphNode> nodeMap = new ConcurrentHashMap<>();
-        SwingView view = new SwingView();
-        ExecutorController controller = new ExecutorController(nodeFactory,nodeMap,view);
-        view.addEventListener(controller);
-    }
+
 }
