79f1fd68709dd8055ef8d0bfed8d3711ab8d48fe
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-25 14:51:51 +0200

    taking term from event+ display update

--- java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
@@ -16,8 +16,6 @@ import java.util.concurrent.ForkJoinPool;
 
 public class ExecutorController implements Controller {
 
-    private final int DEFAULT_DEPTH =2;
-    private int depth;
     private static ForkJoinPool pool;
     private final HttpWikiGraph nodeFactory;
     private final ConcurrentHashMap<String, WikiGraphNode> nodeMap;
@@ -25,36 +23,28 @@ public class ExecutorController implements Controller {
 
     public ExecutorController(HttpWikiGraph nodeFactory, ConcurrentHashMap<String, WikiGraphNode> nodeMap, SwingView view){
         this.view = view;
-        this.depth= DEFAULT_DEPTH;
         this.nodeFactory = nodeFactory;
         this.nodeMap = nodeMap;
 
     }
 
-    public void setDepth(int depth){
-        this.depth=depth;
-    }
-
-    public int getDepth(){
-        return this.depth;
-    }
-    private void compute(String node){
+    private void compute(String node, int depth){
         pool = ForkJoinPool.commonPool();
         this.pool.invoke(
-                new ComputeChildrenTask(null, node,this.depth,this.nodeFactory,this.nodeMap,this.view));
+                new ComputeChildrenTask(null, node,depth,this.nodeFactory,this.nodeMap,this.view,true));
     }
 
     public void exit(){
         this.pool.shutdown();
     }
-    public void computeRandom(){
+    public void computeRandom(int depth){
         WikiGraphNode random = nodeFactory.random();
-        compute(random.term());
+        compute(random.term(),depth);
     }
 
-    public void computeSearch(String node){
+    public void computeSearch(String node, int depth){
         List<Pair<String, String>> res =  this.nodeFactory.search(node);
-        compute(res.get(0).getKey());
+        compute(res.get(0).getKey(),depth);
     }
 
     @Override
@@ -62,11 +52,11 @@ public class ExecutorController implements Controller {
         if(event.getType().equals(ViewEvent.EventType.EXIT)){
             exit();
         }else if (event.getType().equals(ViewEvent.EventType.SEARCH)){
-            computeSearch("term");//get the term
+            computeSearch(event.getText(),event.getDepth());//get the term
         }else if (event.getType().equals(ViewEvent.EventType.RANDOM_SEARCH)){
-            computeRandom();
+            computeRandom(event.getDepth());
         }else if (event.getType().equals(ViewEvent.EventType.OTHER)){
-            compute("term");//get the term
+            //compute("term");//get the term
         }
     }
 
