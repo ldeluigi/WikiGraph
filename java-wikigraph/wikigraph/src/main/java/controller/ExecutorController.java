7dc343dfe30f54b370f2a190969e0e04886ed6d0
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-25 19:19:53 +0200

    fix stuff

--- java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/ExecutorController.java
@@ -14,6 +14,7 @@ import view.ViewEvent;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountedCompleter;
 import java.util.concurrent.ForkJoinPool;
 
 import static java.lang.Runtime.*;
@@ -21,17 +22,17 @@ import static java.lang.Runtime.*;
 public class ExecutorController implements Controller {
 
     private static ForkJoinPool pool;
-    private  HttpWikiGraph nodeFactory;
-    private  ConcurrentHashMap<String, WikiGraphNode> nodeMap;
+    private HttpWikiGraph nodeFactory;
+    private ConcurrentHashMap<String, WikiGraphNode> nodeMap;
     private final View view;
 
-    public ExecutorController(View view){
+    public ExecutorController(View view) {
         this.view = view;
         this.view.addEventListener(this);
     }
 
     @Override
-    public void start(){
+    public void start() {
         this.nodeFactory = new HttpWikiGraph();
         nodeFactory.setLanguage(Locale.ENGLISH.getLanguage());
         this.nodeMap = new ConcurrentHashMap<>();
@@ -39,13 +40,17 @@ public class ExecutorController implements Controller {
         view.start();
     }
 
-    private void compute(String node, int depth){
+    private void compute(String node, int depth) {
+
+        CountedCompleter task = new ComputeChildrenTask(null, node, depth, this.nodeFactory, this.nodeMap, this.view, true);
         this.pool.execute(
-                new ComputeChildrenTask(null, node,depth,this.nodeFactory,this.nodeMap, this.view,true));
+                task);
+
+
     }
 
-    private void exit(){
-        if(this.pool!= null){
+    private void exit() {
+        if (this.pool != null) {
             this.pool.shutdown();
         }
     }
@@ -53,13 +58,17 @@ public class ExecutorController implements Controller {
 
     @Override
     public void notifyEvent(ViewEvent event) {
-        if(event.getType().equals(ViewEvent.EventType.EXIT)){
+        if (event.getType().equals(ViewEvent.EventType.EXIT)) {
             this.exit();
-        }else if (event.getType().equals(ViewEvent.EventType.SEARCH)){
-            compute(event.getText(),event.getDepth());//get the term
-        }else if (event.getType().equals(ViewEvent.EventType.RANDOM_SEARCH)){
-            compute(null,event.getDepth());
-        }else if (event.getType().equals(ViewEvent.EventType.OTHER)){
+        } else if (event.getType().equals(ViewEvent.EventType.SEARCH)) {
+            if (this.pool.isQuiescent()) {
+                compute(event.getText(), event.getDepth());//get the term
+            }
+        } else if (event.getType().equals(ViewEvent.EventType.RANDOM_SEARCH)) {
+            if (this.pool.isQuiescent()) {
+                compute(null, event.getDepth());
+            }
+        } else if (event.getType().equals(ViewEvent.EventType.OTHER)) {
             //compute("term");//get the term
         }
     }
