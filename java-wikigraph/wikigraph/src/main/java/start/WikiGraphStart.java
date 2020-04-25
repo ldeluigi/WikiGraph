7548834486cbb375df4244fa02d9dbfb58b8a003
Simone <Simone.magnani4@studio.unibo.it>
2020-04-25 14:35:47 +0200

    update main

--- java-wikigraph/wikigraph/src/main/java/start/WikiGraphStart.java
+++ java-wikigraph/wikigraph/src/main/java/start/WikiGraphStart.java
@@ -3,7 +3,7 @@ package start;
 import view.SwingView;
 
 public class WikiGraphStart {
-    public static void main(String args[]){
+    public static void main(String[] args){
         new SwingView();
     }
 }
