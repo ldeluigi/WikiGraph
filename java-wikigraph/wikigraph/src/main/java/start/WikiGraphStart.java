bb3706c1dc324316725de11afff410086ba1b0a3
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-25 14:55:00 +0200

    update main

--- java-wikigraph/wikigraph/src/main/java/start/WikiGraphStart.java
+++ java-wikigraph/wikigraph/src/main/java/start/WikiGraphStart.java
@@ -1,9 +1,22 @@
 package start;
 
+import controller.ExecutorController;
+import controller.api.HttpWikiGraph;
+import model.WikiGraphNode;
 import view.SwingView;
 
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class WikiGraphStart {
+
     public static void main(String[] args){
-        new SwingView();
+        HttpWikiGraph nodeFactory = new HttpWikiGraph();
+        nodeFactory.setLanguage(Locale.ENGLISH.getLanguage());
+        ConcurrentHashMap<String, WikiGraphNode> nodeMap = new ConcurrentHashMap<>();
+        SwingView view = new SwingView();
+        ExecutorController controller = new ExecutorController(nodeFactory,nodeMap,view);
+        view.addEventListener(controller);
     }
+
 }
