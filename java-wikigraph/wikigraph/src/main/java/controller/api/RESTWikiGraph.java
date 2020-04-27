0c5fd1472424e69949e59c44aabdc4ec4f208353
Simone <Simone.magnani4@studio.unibo.it>
2020-04-28 01:37:50 +0200

    Hotfixettino biricchino in onore di paolino need to finish test

--- java-wikigraph/wikigraph/src/main/java/controller/api/RESTWikiGraph.java
+++ java-wikigraph/wikigraph/src/main/java/controller/api/RESTWikiGraph.java
@@ -3,12 +3,16 @@ package controller.api;
 import model.WikiGraphNode;
 import model.WikiGraphNodeImpl;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,17 +26,28 @@ public class RESTWikiGraph extends HttpWikiGraph {
     protected WikiGraphNode from(final String term, final String lang) {
         Set<String> sameTerm = new HashSet<>();
         sameTerm.add(term);
-        final String URLTerm = term.replace(" ", "_");
+        String URLTerm = term.replace(" ", "_");
+        try {
+            URLTerm = URLEncoder.encode(URLTerm, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         try {
             Document doc = Jsoup.connect(apiEndpoint(lang) + URLTerm).get();
             if (doc != null) {
                 String termResult = doc.select("html head title").html();
                 Elements links = doc.select("section:first-child p a:not([href*=#])");
                 final Set<String> terms = new HashSet<>();
-                links.forEach((Element link) -> terms.add(link.attr("title")));
+                links.forEach((Element link) -> {
+                    if (link.attr("rel").equals("mw:WikiLink") && !link.attr("href").contains(":")) {
+                        terms.add(link.attr("title"));
+                    }
+                });
+                System.out.println(terms);
                 return new WikiGraphNodeImpl(termResult, sameTerm, terms);
             }
         } catch (IOException e) {
+            System.err.println("from "+ term);
             e.printStackTrace();
         }
         System.err.println("RESTWikiGraph ritorna null");
