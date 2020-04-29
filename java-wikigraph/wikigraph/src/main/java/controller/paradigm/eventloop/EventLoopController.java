575fb1e9fb7b8f0aa092e8085cc8a49e55f6fabf
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-04-29 15:47:38 +0200

    💯💯💯💥🔞✔

--- java-wikigraph/wikigraph/src/main/java/controller/paradigm/eventloop/EventLoopController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/paradigm/eventloop/EventLoopController.java
@@ -1,9 +1,18 @@
 package controller.paradigm.eventloop;
 
 import controller.Controller;
+import view.View;
 import view.ViewEvent;
 
 public class EventLoopController implements Controller {
+
+    private final View view;
+
+    public EventLoopController(View view){
+        this.view=view;
+        view.addEventListener(this);
+
+    }
 
     @Override
     public void notifyEvent(ViewEvent event) {
@@ -12,6 +21,6 @@ public class EventLoopController implements Controller {
 
     @Override
     public void start() {
-
+        view.start();
     }
 }
