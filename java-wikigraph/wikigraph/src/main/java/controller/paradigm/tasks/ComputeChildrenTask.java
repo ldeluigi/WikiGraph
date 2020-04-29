2b6131a60c6bd166228b756a070eed24372ee0ce
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-04-29 12:41:11 +0200

    Added recursion abstraction

--- java-wikigraph/wikigraph/src/main/java/controller/paradigm/tasks/ComputeChildrenTask.java
+++ java-wikigraph/wikigraph/src/main/java/controller/paradigm/tasks/ComputeChildrenTask.java
@@ -2,7 +2,9 @@ package controller.paradigm.tasks;
 
 import controller.ConcurrentWikiGraph;
 import controller.api.HttpWikiGraph;
+import controller.paradigm.NodeRecursion;
 import model.WikiGraphNode;
+import model.WikiGraphNodeFactory;
 import view.View;
 
 import java.util.concurrent.CountedCompleter;
@@ -11,69 +13,21 @@ import java.util.concurrent.locks.Lock;
 
 public class ComputeChildrenTask extends CountedCompleter<Void> {
 
+    private final NodeRecursion nr;
 
-    private final String myTerm;
-    private final int myDepth;
-    private final HttpWikiGraph nodeFactory;
-    private final ConcurrentWikiGraph graph;
-    private final View view;
-    private final int maxDepth;
-    private final String fatherId;
-    private String id;
-
-    public ComputeChildrenTask(ComputeChildrenTask father, String term, int depth, HttpWikiGraph nodeFactory, ConcurrentWikiGraph graph, View view, int maxdepth) {
+    private ComputeChildrenTask(final ComputeChildrenTask father, final NodeRecursion rec) {
         super(father);
-        this.nodeFactory = nodeFactory;
-        this.myTerm = term;
-        this.myDepth = depth;
-        this.graph = graph;
-        this.view = view;
-        this.maxDepth = maxdepth;
-        this.fatherId = father == null ? "" : father.getNodeId();
+        this.nr = rec;
     }
 
+    public ComputeChildrenTask(WikiGraphNodeFactory factory, ConcurrentWikiGraph graph,
+                               View view, int maxDepth, String term) {
+        this.nr = new TaskNodeRecursion(factory, graph, view, maxDepth, term);
+    }
 
     @Override
     public void compute() {
-        if (this.graph.isAborted()) {
-            tryComplete();
-            return;
-        }
-        final WikiGraphNode result;
-        if (this.myDepth == 0) {
-            if (this.myTerm == null) { //random
-                result = nodeFactory.random();
-            } else { //search
-                result = this.nodeFactory.from(this.myTerm);
-            }
-        } else {
-            result = nodeFactory.from(this.myTerm);
-        }
-        if (result != null) {
-            this.id = result.term();
-            final Lock lock = this.graph.getLockOn(this.id);
-            lock.lock();
-            try {
-                if (this.graph.contains(this.id)) {
-                    this.view.addEdge(this.fatherId, this.id);
-                } else {
-                    this.view.addNode(this.id, this.myDepth, this.nodeFactory.getLanguage());
-                    this.graph.add(result);
-                    if (this.myDepth > 0) {
-                        view.addEdge(this.fatherId, this.id);
-                    }
-                    if (this.myDepth < this.maxDepth) {
-                        for (String child : result.childrenTerms()) {
-                            addToPendingCount(1);
-                            new ComputeChildrenTask(this, child, this.myDepth + 1, this.nodeFactory, this.graph, this.view, maxDepth).fork();
-                        }
-                    }
-                }
-            } finally {
-                lock.unlock();
-            }
-        }
-        tryComplete();
+        this.nr.compute();
     }
 
     @Override
@@ -82,7 +36,32 @@ public class ComputeChildrenTask extends CountedCompleter<Void> {
         return false;
     }
 
-    public String getNodeId() {
-        return this.id;
+    private class TaskNodeRecursion extends NodeRecursion {
+
+        private TaskNodeRecursion(NodeRecursion father, String term) {
+            super(father, term);
+        }
+
+        public TaskNodeRecursion(WikiGraphNodeFactory factory, ConcurrentWikiGraph graph,
+                                 View view, int maxDepth, String term) {
+            super(factory, graph, view, maxDepth, term);
+        }
+
+        @Override
+        protected void complete() {
+            tryComplete();
+        }
+
+        @Override
+        protected void scheduleChild(String term) {
+            addToPendingCount(1);
+            new ComputeChildrenTask(ComputeChildrenTask.this,
+                    new TaskNodeRecursion(this, term)).fork();
+        }
+
+        @Override
+        public void abort() {
+            tryComplete();
+        }
     }
 }
