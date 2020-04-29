4308054f30cdd422c1960b04c78b9d3d1f6ce3ea
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-04-29 15:56:06 +0200

    Added stats 📈

--- java-wikigraph/wikigraph/src/main/java/controller/paradigm/eventloop/EventLoopController.java
+++ java-wikigraph/wikigraph/src/main/java/controller/paradigm/eventloop/EventLoopController.java
@@ -1,11 +1,20 @@
 package controller.paradigm.eventloop;
 
 import controller.Controller;
+import view.View;
 import view.ViewEvent;
 
 import java.util.concurrent.BlockingQueue;
 
 public class EventLoopController implements Controller {
+
+    private final View view;
+
+    public EventLoopController(View view){
+        this.view=view;
+        view.addEventListener(this);
+
+    }
 
     @Override
     public void notifyEvent(final ViewEvent event) {
@@ -14,6 +23,6 @@ public class EventLoopController implements Controller {
 
     @Override
     public void start() {
-
+        view.start();
     }
 }
