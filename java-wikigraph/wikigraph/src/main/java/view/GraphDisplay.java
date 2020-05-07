2e36aad9a449e2bf40b606663fb52591869f02cc
Simone <Simone.magnani4@studio.unibo.it>
2020-05-07 15:44:33 +0200

    GraphDisplay has new method to update depth of nodes

--- java-wikigraph/wikigraph/src/main/java/view/GraphDisplay.java
+++ java-wikigraph/wikigraph/src/main/java/view/GraphDisplay.java
@@ -1,7 +1,13 @@
 package view;
 
+import org.graphstream.stream.SourceBase;
+
+import java.lang.annotation.ElementType;
+
 public interface GraphDisplay {
     void addNode(final String id, final int depth, final String lang);
+
+    void updateDepthNode(String id, int depth);
 
     void addEdge(final String idFrom, final String idTo);
 
